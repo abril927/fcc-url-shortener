f9a4813b7b2ec22442a101e22626ed9b47cf7823
Abril <abril927@proton.me>
2024-03-10 17:24:59 +0100

    Add database

---
+++ README.md
@@ -0,0 +1,17 @@
+# fcc-url-shortener
+
+This is one of my projects for the [freeCodeCamp back-end development certification](https://www.freecodecamp.org/learn/back-end-development-and-apis/).
+
+## Setup
+
+```bash
+# Setting up for the first time
+yarn install
+yarn run db:push
+
+# When the code changes
+yarn build
+
+# Run the project
+yarn start
+```
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { prisma } from './database';
+import { ShortURL } from '@prisma/client';
+
 import { default as express, Express } from 'express';
 import { default as cors } from 'cors';
 import { default as BodyParser } from 'body-parser';
@@ -10,17 +13,24 @@ import { default as dns } from 'dns';
 let app = express();
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.get('/', (req, res) => {
-	res.send('API: GET /api/shorturl/:id, POST /api/shorturl');
+app.get('/', async (req, res) => {
+	const count = await prisma.shortURL.count();
+	res.send(`There are currently ${count} short URLs. API: GET /api/shorturl/:id, POST /api/shorturl`);
 });
 
 // Status codes are commented out because freeCodeCamp fails the test if they're there...
 
-let shortlinks: string[] = [];
-app.get('/api/shorturl/:id', (req, res) => {
+app.get('/api/shorturl/:id', async (req, res) => {
 	const id = parseInt(req.params.id);
-	if (id in shortlinks) {
-		return res.redirect(shortlinks[id]);
+	if (isNaN(id)) return res/*.status(401)*/.json({ error: 'Invalid ID' });
+	const shortURL = await prisma.shortURL.findUnique({
+		where: {
+			id: id
+		}
+	});
+
+	if (shortURL != null) {
+		return res.redirect(shortURL.url);
 	} else {
 		return res/*.status(404)*/.json({ error: 'ID not found' });
 	}
@@ -36,13 +46,25 @@ app.post('/api/shorturl', BodyParser.urlencoded({ extended: false }), async (req
 		return res/*.status(400)*/.json({ error: 'invalid url' });
 	}
 	
-	dns.lookup(parsed.hostname, (err) => {
+	dns.lookup(parsed.hostname, async (err) => {
 		if (err) {
 			return res/*.status(400)*/.json({ error: 'invalid url' });
 		} else {
+			let newShortURL: ShortURL;
+			try {
+				newShortURL = await prisma.shortURL.create({
+					data: {
+						url: parsed.toString()
+					}
+				});
+			} catch(reason) {
+				console.error('error during shorturl creation: ' + reason);
+				return res.status(500).json({ error: 'an internal database error occured' });
+			}
+
 			return res.json({
 				original_url: parsed.toString(),
-				short_url: shortlinks.push(parsed.toString()) - 1 // off by one
+				short_url: newShortURL.id
 			});
 		}
 	});
